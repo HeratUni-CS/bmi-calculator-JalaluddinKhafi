7601239213feaf1e6895cc40a043a80d2866492b
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-05-31 17:27:02 +0430

    font awesome added to project

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
-const bottomContainerColor= Color(0xFFEB1555);
-const actCardColor =Color(0xFF111428);
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+const bottomContainerColor = Color(0xFFEB1555);
+const actCardColor = Color(0xFF111428);
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,50 +12,73 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Column(
-        children: [
-          Expanded(child:Row(
-            children: [
-              reusibleW(color: actCardColor),
-              reusibleW(color: actCardColor),
-            ],
-          )),
-          reusibleW(color: actCardColor),
-          Expanded(child: Row(
-            children: [
-             reusibleW(color: actCardColor),
-              reusibleW(color: actCardColor),
-            ],
-          ),
-          ),
-          Container(
-            height: 80,
-            color: bottomContainerColor,
-            margin: EdgeInsets.only(top: 15),
-          )
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                reusibleW(color: actCardColor,
+                  child: Column(
+                    children: [
+                      Icon(FontAwesomeIcons.mars)
+                    ],
+                  ),
+
+                ),
+                reusibleW(color: actCardColor,
+
+                ),
+              ],
+            ),
+            ),
+            reusibleW(color: actCardColor,
+              child: Column(
+                children: [
+
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  reusibleW(color: actCardColor,
+
+                  ),
+                  reusibleW(color: actCardColor,
+
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 80,
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 15),
+            )
+          ],
+        ));
   }
 }
 
 class reusibleW extends StatelessWidget {
+  reusibleW({required this.color, this.child});
 
-  reusibleW({required this.color});
-
-final Color color;
-
+  final Color color;
+  final Widget? child;
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: Container(
-      margin: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color:color,
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color,
+        ),
+        child:child ,
       ),
-    ));
+    );
   }
 }

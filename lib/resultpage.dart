cfba977666b68dd4f52342654040a01c1606ef36
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-01 19:07:41 +0430

    BottomButton for next page added

--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -1,8 +1,9 @@
+import 'package:bmi_starting/bottomButton.dart';
 import 'package:flutter/material.dart';
 import 'package:bmi_starting/constant.dart';
 import 'package:bmi_starting/ReusFiles.dart';
-class resultPage extends StatelessWidget {
 
+class resultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,36 +14,45 @@ class resultPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15),
-            alignment: Alignment.bottomLeft,
-            child: Text(
-              'Your Result',
-              style: kTitleStyle,
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: kTitleStyle,
+              ),
             ),
-            
-          ),),
+          ),
           Expanded(
             flex: 5,
-              child: reusibleW(
+            child: reusibleW(
                 color: kactCardColor,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("NORMAL",
-                    style: kResultTextStyle,
+                    Text(
+                      "NORMAL",
+                      style: kResultTextStyle,
                     ),
-                    Text("25",
-                    style: kBMITextStyle,),
-                    Text('You are goot to go',
-                    style: kBodyTextStyle,),
-
+                    Text(
+                      "25",
+                      style: kBMITextStyle,
+                    ),
+                    Text(
+                      'You are goot to go',
+                      style: kBodyTextStyle,
+                    ),
                   ],
-                )
-              ),
-
-          )
+                )),
+          ),
+          BottomButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            label: 'RECALCULATOR',
+          ),
         ],
       ),
     );
